7497eef9c297e4f2bd998f171e893846d33e69ef
fwolff <fwolff@graniteds.org>
2014-06-27 14:20:19 +0200

    Added missing headers.

--- src/test/java/org/spearal/jpa2/TestEntity.java
+++ src/test/java/org/spearal/jpa2/TestEntity.java
@@ -1,3 +1,20 @@
+/**
+ * == @Spearal ==>
+ * 
+ * Copyright (C) 2014 Franck WOLFF & William DRAI (http://www.spearal.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.spearal.jpa2;
 
 import java.io.ByteArrayInputStream;
@@ -19,6 +36,9 @@ import org.spearal.configuration.PartialObjectFactory.UndefinedPropertyException
 import org.spearal.jpa2.model.Person;
 import org.speral.jpa2.descriptor.EntityDescriptorFactory;
 
+/**
+ * @author Franck WOLFF
+ */
 public class TestEntity extends AbstractHibernate4TestUnit {
 
 	@Before
